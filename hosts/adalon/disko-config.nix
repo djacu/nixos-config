e09a70dadaaccb34d15436783890e923d9662847
Daniel Baker <daniel.n.baker@gmail.com>
2023-10-17 21:02:00 -0700

    clean up adalon host disko config for install.

--- hosts/adalon/disko-config.nix
+++ hosts/adalon/disko-config.nix
@@ -1,8 +1,6 @@
 let
-  disks = [
-    "nvme-WD_BLACK_SN850_1TB_204178806629"
-  ];
+  device = "/dev/disk/by-id/nvme-WD_BLACK_SN850_1TB_204178806629";
 
   disko-config = import ../../disko;
 in
-  disko-config {inherit disks;}
+  disko-config {inherit device;}
