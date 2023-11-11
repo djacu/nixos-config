7ae836a63d3b481ccbf1711ba7e9e53750d10110
Daniel Baker <daniel.baker@tweag.io>
2023-11-11 11:10:21 -0800

    Turn off mutableUsers and add a default root password.

--- hosts/adalon/configuration.nix
+++ hosts/adalon/configuration.nix
@@ -38,4 +38,7 @@
   time.timeZone = "America/Los_Angeles";
 
   system.stateVersion = "23.11";
+
+  users.mutableUsers = false;
+  users.users.root.hashedPassword = "$y$j9T$O74BbKKdx3dYDjAQzsY1Z0$uu/V4AurhJpstdm7.1o4HQvpakZIZapVi5uYICfsOA0";
 }
