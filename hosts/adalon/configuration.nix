b102c9a2fc5b0036fb18c5a5efd2c32e23d55816
Daniel Baker <daniel.baker@tweag.io>
2023-11-12 14:52:45 -0800

    Remove old test password from root and add real hashed password to my user.

--- hosts/adalon/configuration.nix
+++ hosts/adalon/configuration.nix
@@ -41,5 +41,4 @@
 
   users.mutableUsers = false;
   users.users.root.initialHashedPassword = "$6$efX.JpKjAey2jrYG$kOt..AuFrPPIVTDncVj7vNkIo4MR/9mYG2SaDV2xpSNDEmk8DRxVNmuMI6hcW.CmD6ZDqdIKCj2MAyHnIdrkl/";
-  # users.users.root.initialPassword = "hunter2";
 }
--- users/bakerdn/default.nix
+++ users/bakerdn/default.nix
@@ -2,7 +2,7 @@
   nix.settings.trusted-users = ["bakerdn"];
   users.users.bakerdn = {
     isNormalUser = true;
-    initialPassword = "password";
+    initialHashedPassword = "$6$zo7JRVBVVKUVn047$IbDRNzWNcp1hwO2jFGnH9PbGYh2fwDP52zc1/ggMovCYiuMg7N2d7NcCkAr8//Yc6s66D4/tzt8BuuM1Nap3F1";
     extraGroups = [
       "wheel"
       "networkManager"
